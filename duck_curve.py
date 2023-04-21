ea981ea349a76f8371c12f0c5896696b793e882e
Brendan Chermack <Chermackbrendan@gmail.com>
2023-04-21 18:19:53 -0500

    reworked functions

--- duck_curve.py
+++ duck_curve.py
@@ -11,13 +11,18 @@ def total_grid_gen():
     return total
 ###########################
 
-##RTU stuff
-def net_load(): # works with RTU files potentional generation to help balance the total load
-    return net_load 
-def net_generation():# works with RTU files potentional load to help balance the total generation
-    return net_generation
+##power managing functions##
+def can_discharge(): # if the total load is greater than the total generation then the RTU can discharge to the grid
+    return can_discharge
+def can_charge(): # if the total generation is greater than the total load then the RTU can charge from the grid
+    return can_charge
+
+def can_sell_power():# during the hr 6pm to early morning power can be sold to the grid
+    return can_sell_power
+
+
 def idle():# if total generation adn load and equal then nothing will happen with the RTUs
-    if(net_load == net_generation):
+    if(total_grid_load() == total_grid_gen()):
         return 0
 def generation_of_power():
     return generation_of_power
