a170d49874dc83abec2a1ea99123fe603d7852a6
Brendan Chermack <Chermackbrendan@gmail.com>
2023-04-21 15:09:28 -0500

    added function

--- duck_curve.py
+++ duck_curve.py
@@ -3,4 +3,11 @@ with open('mock_data.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     for row in csvreader:
         print (row)
+        
+def total_load():
+    return total_load
+def total_generation():
+    return total_generation
+def generation_of_power():
+    return generation_of_power
     
